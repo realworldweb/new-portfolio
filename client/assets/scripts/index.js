a90b636cf114d3ab3a93d546a56049ba7b21778c
paul <paulrooney60@gmail.com>
2020-09-09 18:16:23 +0100

    minor update

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -11,7 +11,7 @@ let certsList;
 const loadData = async () =>{
  
 try {
-  const response = await Axios.get('/.netlify/functions/server/projectsapi/certs/');
+  const response = await Axios.get('/.netlify/functions/server/certs');
   certsList = response.data;
   const certsContainer = document.getElementById('certsContainer')
   certsContainer.innerHTML =`<div class="d-flex flex-wra gallery justify-content-center">
