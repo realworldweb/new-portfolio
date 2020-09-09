4a0414d7e9f71e17fba3a3e85dfc789f02e85d46
paul <paulrooney60@gmail.com>
2020-09-10 00:07:52 +0100

    experimenting with touchmove event

--- client/assets/scripts/modules/simplegallery.js
+++ client/assets/scripts/modules/simplegallery.js
@@ -54,8 +54,8 @@ class Gallery{
   }
 	}
  
- handleTouch(e){
-  console.log(ran)
+ handleTouchMove(e){
+  console.log('ran')
   this.touched = e.touches[0].clientX
   
   if(this.touched > this.touchedlast){
