a0c7cb90e7fb34d9e33fd1c0d533e3be0bd0213a
paul <paulrooney60@gmail.com>
2020-09-15 20:18:33 +0100

    added feedback get command

--- server/server.js
+++ server/server.js
@@ -15,14 +15,26 @@ const url = process.env.mongoServer;//authenticate
 let projectsList
 let certsList
 
-async function main (){
+async function main (get){
  
  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true})
  
  try{
  await client.connect()
+ if( get === projects){
  await projectsMount(client);//mount projects so we can run various requests
+ return
+ }
+  if( get === certs){
  await certsMount(client);//mount certs so we can run various requests
+ return
+  }
+  
+  if( get === feedback){
+   await feedbackMount(client);
+   return
+  }
+  
  }catch(e){
   console.error(e)
  }
@@ -40,6 +52,13 @@ async function main (){
  async function certsMount(client){// load certs and pass it to express route for processing.
  console.log('ran')
    certsList = await client.db('portfolio').collection('courses')
+   
+   
+ }
+ 
+  async function feedbackMount(client){// load certs and pass it to express route for processing.
+ console.log('ran')
+   feedbackList = await client.db('portfolio').collection('feedback')
    
    
  }
@@ -62,7 +81,7 @@ async function insert (feedback){
  
  async function feedbackInsert(client, feedback){// load certs and pass it to express route for processing.
  console.log('inserted')
-   feedbackList = await client.db('portfolio').collection('feedback')
+   const feedbackList = await client.db('portfolio').collection('feedback')
    
    const data = await feedbackList.insertOne(feedback)
  }
@@ -75,7 +94,7 @@ async function insert (feedback){
 app.use(cors());
 
 router.get('/projects', async (req, res) => {// return all from api projects
- await main().catch(console.error)// call main to start mongo
+ await main('projects').catch(console.error)// call main to start mongo
   const data = await projectsList.find({}).toArray()
   
   res.json(data);
@@ -85,7 +104,7 @@ router.get('/projects', async (req, res) => {// return all from api projects
 });
 
 router.get('/projects/:id', async (req, res) => {// query projects api 
-  await main().catch(console.error)// call main to start mongo
+  await main('projects').catch(console.error)// call main to start mongo
   const data = await projectsList.find({"name" : req.params.name})
   
   res.json(data);
@@ -95,7 +114,7 @@ router.get('/projects/:id', async (req, res) => {// query projects api
 });
 
 router.get('/certs', async (req, res) => {//return all certs from api 
- await main().catch(console.error);// call main to start mongo
+ await main('certs').catch(console.error);// call main to start mongo
  
   const data = await certsList.find({}).toArray()
   
@@ -106,8 +125,18 @@ router.get('/certs', async (req, res) => {//return all certs from api
 });
 
 router.get('/certs/:id', async (req, res) => {//return all certs from api 
- await main().catch(console.error)// call main to start mongo
+ await main('certs').catch(console.error)// call main to start mongo
   const data = await certsList.find({}).toArray()
+  
+  res.json(data);
+
+ 
+  
+});
+
+router.get('/feedback', async (req, res) => {//return all certs from api 
+ await main('feedback').catch(console.error)// call main to start mongo
+  const data = await feedbackList.find({}).toArray()
   
   res.json(data);
 
