cab5aa021342bfc0c3d9aff58515cb997b6142cb
paul <paulrooney60@gmail.com>
2020-09-16 02:33:52 +0100

    finished feedback page full responsive draft

--- client/assets/scripts/modules/getfeedback.js
+++ client/assets/scripts/modules/getfeedback.js
@@ -2,6 +2,7 @@ class GetFeedback{
  constructor(feedbackList){
   this.feedbackStage = document.getElementById('feedbackStage')
   this.feedback = feedbackList
+  this.feedbackLimit = feedbackList.slice(feedbackList.length -2)
   
    
    
@@ -15,17 +16,19 @@ class GetFeedback{
 loadFeedback(){
 
  
-    for(const feedback of this.feedback){//add default thumb selection
-      console.log(feedback)
-     this.feedbackStage.innerHTML +=`<p>Topic:&nbsp;${feedback.about}</p>
-     <div>
-     <p>Feedback:<br>${feedback.body}</p>
-     <p><i>Name:&nbsp;${feedback.name}</i></p>
-     <p><i>Created on:&nbsp;${feedback.createdOn}</i></p>
-     </div>`
+    for(const feedback of this.feedbackLimit){//add default thumb selection
+ 
+     this.feedbackStage.innerHTML +=`
+     <div class="d-inline-block position-relative text-center mx-auto mt-3 p-4 feedback-display feedback-display--limited">
+     <p class="feedback-display__topic"><span class=" float-left feedback-display__feedback-label">Topic:&nbsp;</span><br>${feedback.about}</p>
+     <p class="feedback-display__feedback"><span class=" float-left feedback-display__feedback-label">Feedback:</span><br>${feedback.body}</p>
+     <p class="d-inline mr-5 feedback-display__name"><i>Name:&nbsp;${feedback.name}</i></p>
+     <p class="d-inline ml-5 feedback-display__created-on"><i>Created on:&nbsp;${feedback.createdOn}</i></p>
+     </div><br>`
     
     
     }
+    this.feedbackStage.innerHTML +=`<a href="allfeedback.html" class="d-block text-right m-5 feedback__link">See All Feedback<i class="fas fa-angle-double-right ml-2"></i></a>`
     
   }
 }
