0a5d9403efacd149e43d048bf930f28ebc9f8983
paul <paulrooney60@gmail.com>
2020-09-18 01:10:20 +0100

    fixed layout chages in gallery

--- client/assets/scripts/contact.js
+++
@@ -1,8 +0,0 @@
-import ContactUs from './modules/form.js'
-
-const contactDetails = document.getElementById('contactDetails')
-
-if(contactDetails !== null){
-
-const contact  = new ContactUs();
-}
--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -19,14 +19,14 @@ const certsInfo = await loadCerts()
 
 certsContainer.innerHTML =`<div class="d-flex flex-wrap gallery justify-content-center">
   <div class="row">
-<i id="control-left" class="fas fa-caret-down left"></i>
+<i id="control-left" class="fas fa-caret-left caret"></i>
 <picture class="">
 <source id="large" sizes="1000px" srcset="assets/images/${certsInfo[certsInfo.length-6].certificate.large.url} 1100w" media="(min-width: 1200px)">
 <source id="medium" class="medium" sizes="700px" srcset="assets/images/${certsInfo[certsInfo.length-6].certificate.medium.url} 580w" media="(min-width: 1000px)">
 <source id="mediumxl" class="medium"  sizes="700px" srcset="assets/images/${certsInfo[certsInfo.length-6].certificate.medium.url} 780w" media="(min-width: 700px)">
 <img sizes="500px" srcset="assets/images/${certsInfo[certsInfo.length-6].certificate.small.url} 600w" src="assets/images/${certsInfo[certsInfo.length-6].certificate.small.url}" alt="${certsInfo[certsInfo.length-6].certificate.alt}" class="d-block mx-auto text-center gallery__main-pic">
 </picture>
-<i id="control-right" class="fas fa-caret-down right"></i>
+<i id="control-right" class="fas fa-caret-right caret caret--right"></i>
 </div>
 </div>
 `
