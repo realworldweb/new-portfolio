80186d099d4133dec4676d97b9dc794a57e826a3
paul <paulrooney60@gmail.com>
2020-09-20 14:57:44 +0100

    finished bottstrap-nav

--- client/assets/scripts/modules/mobilenav.js
+++ client/assets/scripts/modules/mobilenav.js
@@ -3,6 +3,7 @@ class MobileNav{
  this.toggler = document.querySelector('.header__toggle')
  this.togglerArea = this.toggler.getAttribute('data-target')
  this.togglerState = this.toggler.getAttribute('aria-expanded')
+ this.load = true
  this.events()
 
  
@@ -15,30 +16,37 @@ this.toggler.addEventListener('click', e =>  this.run(e))
  
  
 run(){
- 
+
  const head = this.togglerArea
  const childEl = document.querySelector(head).children
- for(const el of childEl){
+ if( this.load === true)
+ for(const el of childEl){//set intial values for all children
  
-  el.style.transition = 'opacity 2s'
+  el.style.transition = 'opacity 4s'
+  el.style.opacity = 0
   
  }
  if(this.togglerState === 'false'){
   
   
   this.togglerState = 'true'
-  document.querySelector('.header').classList.remove('header--navclosed')
+  document.querySelector('.header').classList.remove('header--navclosed')//open header
   
   
- setTimeout(function(){
+
+  setTimeout(function(){//reveal area nav
   document.querySelector(head).classList.remove('d-none') 
-  document.querySelector(head).classList.add('d-flex')
+ document.querySelector(head).classList.add('d-flex') 
+ }, 150)
   
-  }, 400)
+ setTimeout(function(){//reveal area nav
   
-  for(const el of childEl){
+  for(const el of childEl){//reveal children 
   el.style.opacity = 1
   }
+  }, 250)
+  
+  
 
   
   
@@ -46,14 +54,14 @@ run(){
  }else{
   this.togglerState = 'false'
   
-    for(const el of childEl){
+    for(const el of childEl){//hide children
  el.style.opacity = 0
     }
   setTimeout(function(){
- document.querySelector('.header').classList.add('header--navclosed')
+ document.querySelector('.header').classList.add('header--navclosed')//close header
   }, 100)
   
-  setTimeout(function(){
+  setTimeout(function(){//hide area nav
   document.querySelector(head).classList.add('d-none')
   document.querySelector(head).classList.remove('d-flex')
   }, 600)
