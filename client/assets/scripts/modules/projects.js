7c1a120b14090852ca330cf980bc2459d257e697
paul <paulrooney60@gmail.com>
2022-03-30 21:20:19 +0100

    updated responsive images projects and certs fixed header for xs viewport

--- client/assets/scripts/modules/projects.js
+++ client/assets/scripts/modules/projects.js
@@ -58,7 +58,7 @@ loadProjects(){
  <picture>
   <source sizes="1000px" srcset="assets/images/${project[0].img.medium.url} 1200w" media="(min-width: 2200px)">
   <source sizes="700px" srcset="assets/images/${project[0].img.medium.url} 800w" media="(min-width: 1800px)">
-  <img sizes="500px"  srcset="assets/images/${project[0].img.small.url} 650w" src="assets/images/${project[0].img.small.url}" class="card-img rounded" alt="${project[0].img.alt}">
+  <img sizes="500px"  srcset="assets/images/${project[0].img.small.url} 650w" src="assets/images/${project[0].img.small.url}" class="card-img rounded" alt="${project[0].img.alt}" width="500" height="350">
  </picture>
   <div class="position-absolute projects__overlay">
   <p class="position-relative d-block mx-auto text-center"><i>${project[0].name}</i></p>
