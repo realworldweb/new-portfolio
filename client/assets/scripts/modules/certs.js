584d368f1a8c98a97272b7d12efaf23c1e499294
paul <paulrooney60@gmail.com>
2022-03-30 17:47:31 +0100

    updated certs page images

--- client/assets/scripts/modules/certs.js
+++ client/assets/scripts/modules/certs.js
@@ -58,7 +58,7 @@ loadCerts(){
  <picture>
   <source sizes="1000px" srcset="assets/images/${cert[0].img.large.url} 1200w" media="(min-width: 2200px)">
   <source sizes="700px" srcset="assets/images/${cert[0].img.medium.url} 800w" media="(min-width: 1800px)">
-  <img sizes="500px"  srcset="assets/images/${cert[0].img.small.url} 650w" src="assets/images/${cert[0].img.small.url}" class="card-img rounded" alt="${cert[0].img.alt}">
+  <img sizes="500px"  srcset="assets/images/${cert[0].img.small.url} 650w" src="assets/images/${cert[0].img.small.url}" class="card-img rounded" width="500" height="350" alt="${cert[0].img.alt}">
  </picture>
   <div class="position-absolute projects__overlay">
   <p class="position-relative d-block mx-auto text-center"><i>${cert[0].name}</i></p>
